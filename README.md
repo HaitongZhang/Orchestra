e13697f184104a2f68d10abc006f0d0ec1d1adec
张海桐 <zhanghaitong01@corp.netease.com>
2021-10-11 10:02:05 +0800

    few-shot-VL

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Orchestra
+# Improve few-shot voice cloning using multi-modal learning
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 window.onload = function() {
-      document.getElementById('main-content').innerHTML = 'Audio samples from "Orchestra: A Multi-modal Voice Cloning System"'
+      document.getElementById('main-content').innerHTML = 'Audio samples from "Improve few-shot voice cloning using multi-modal learning"'
       
 }
